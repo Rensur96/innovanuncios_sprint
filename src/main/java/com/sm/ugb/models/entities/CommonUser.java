f1e12e0b8ebe7193ddcdd47335b6be9366d66d22
Alex Sura <renealexanders9@gmail.com>
2020-02-29 14:49:27 -0600

    entidad CommonUser

--- src/main/java/com/sm/ugb/models/entities/CommonUser.java
+++ src/main/java/com/sm/ugb/models/entities/CommonUser.java
@@ -1,5 +1,43 @@
 package com.sm.ugb.models.entities;
 
-public class CommonUser {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="common_user")
+public class CommonUser implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name= "user_id", nullable=false)
+	private UsersList userList;
+	
+	@Column(name="ulist_user_id")
+	@Id
+	private long ulistUserId;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="apellido")
+	private String apellido;
+	
+	@Column(name="address")
+	private String address;
+	
+	@Column(name="cellphone")
+	private char[] cellphone;
+	
+	
 }
