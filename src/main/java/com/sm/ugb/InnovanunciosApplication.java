7974c3cc0584091164dbb0ea2fe644e01e8da4a1
Alex Sura <renealexanders9@gmail.com>
2020-02-28 19:57:02 -0600

    Creacion de proyecto

---
+++ src/main/java/com/sm/ugb/InnovanunciosApplication.java
@@ -0,0 +1,13 @@
+package com.sm.ugb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovanunciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovanunciosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sm/ugb/InnovanunciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.ugb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovanunciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
