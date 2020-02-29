d20134984119ab9dce5c52bbc164615107f1877a
Alex Sura <renealexanders9@gmail.com>
2020-02-28 21:20:00 -0600

    UserList agregado

--- src/main/java/com/sm/ugb/models/entities/UsersList.java
+++ src/main/java/com/sm/ugb/models/entities/UsersList.java
@@ -1,5 +1,37 @@
 package com.sm.ugb.models.entities;
 
-public class UsersList {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
+@Entity
+@Table(name="Users_list")
+public class UsersList {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long userId;
+	
+	@Column(name="user")
+	@NotEmpty
+	private String user;
+	
+	@Column(name="passwd")
+	@NotEmpty
+	private String passwd;
+	
+	@Column(name="email")
+	@NotEmpty
+	private String email;
+	
+	@Column(name="levelRole")
+	@NotEmpty
+	private String levelRole;
+	
+	@Column(name="isActive")
+	@NotEmpty
+	private boolean isActive;
 }
