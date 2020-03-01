53c33db1ec0fd73b3233984a00ee1342406bf159
Alex Sura <renealexanders9@gmail.com>
2020-02-29 21:18:42 -0600

    entidades point y reward

--- src/main/java/com/sm/ugb/models/entities/Point.java
+++ src/main/java/com/sm/ugb/models/entities/Point.java
@@ -1,5 +1,47 @@
 package com.sm.ugb.models.entities;
 
-public class Point {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="points")
+public class Point implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name="ulist_user_id")
+	private long ulistUserId;
+	
+	@Column(name="advertiser_id")
+	private long advertiserId;
+	
+	@Column(name="points")
+	private int points;
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	public long getUlistUserId() {
+		return ulistUserId;
+	}
+
+	public long getAdvertiserId() {
+		return advertiserId;
+	}
+	
+	
 
 }
--- src/main/java/com/sm/ugb/models/entities/Reward.java
+++ src/main/java/com/sm/ugb/models/entities/Reward.java
@@ -1,5 +1,104 @@
 package com.sm.ugb.models.entities;
 
-public class Reward {
+import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="rewards")
+public class Reward implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name="reward_id")
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private long rewardId;
+	
+	@Column(name="ulist_advertiser_id")
+	private long ulistAdvertiserId;
+	
+	@Column(name="points_required")
+	private int pointsRequired;
+	
+	@Column(name="reward_name")
+	private String rewardName;
+	
+	@Column(name="reward_description")
+	private String rewardDescription;
+	
+	@Column(name="date_published")
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date datePublished;
+	
+	@Column(name="is_active")
+	private boolean isActive;
+
+	public int getPointsRequired() {
+		return pointsRequired;
+	}
+
+	public void setPointsRequired(int pointsRequired) {
+		this.pointsRequired = pointsRequired;
+	}
+
+	public String getRewardName() {
+		return rewardName;
+	}
+
+	public void setRewardName(String rewardName) {
+		this.rewardName = rewardName;
+	}
+
+	public String getRewardDescription() {
+		return rewardDescription;
+	}
+
+	public void setRewardDescription(String rewardDescription) {
+		this.rewardDescription = rewardDescription;
+	}
+
+	public Date getDatePublished() {
+		return datePublished;
+	}
+
+	public void setDatePublished(Date datePublished) {
+		this.datePublished = datePublished;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public long getRewardId() {
+		return rewardId;
+	}
+
+	public long getUlistAdvertiserId() {
+		return ulistAdvertiserId;
+	}
+	
+	
 }
