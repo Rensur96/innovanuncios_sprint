b3bbb203a1e0fe027454a7c1ccd9cd46045356b1
Mauricio Cruz <m97cruz@gmail.com>
2020-03-14 20:52:14 -0600

    Resolucion de conflictos para sincronizar version final

--- src/main/java/com/sm/ugb/models/services/PaymentService.java
+++ src/main/java/com/sm/ugb/models/services/PaymentService.java
@@ -4,14 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import com.sm.ugb.models.entities.Payment;
-<<<<<<< HEAD
 
 public interface PaymentService {
-=======
-
-public interface PaymentService {
-
->>>>>>> 629e1f38ade34cba13e12a78bde5d9b84e09a5a3
 	
 	public List<Payment> findAll();
 	
@@ -20,9 +14,5 @@ public interface PaymentService {
 	public Optional<Payment> findById(Long id);
 	
 	public void delete(Long id);
-<<<<<<< HEAD
-	
+
 }
-=======
-}
->>>>>>> 629e1f38ade34cba13e12a78bde5d9b84e09a5a3
